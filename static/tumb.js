11f87350c3d8d54be1f8e3f4c05af31f856e1b6d
unknown <bhe.rifai07@gmail.com>
2024-02-10 02:04:30 +0700

    update Sat, Feb 10, 2024  2:04:30 AM

--- static/tumb.js
+++ static/tumb.js
@@ -2,18 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
 
-let directory_name = 'input-head';
+let directory_name = 'inputheader';
 
 let filenames = fs.readdirSync(directory_name);
 
 filenames.forEach((file) => {
   const fileFormat = getExtension(file);
+  const fileName = getName(file);
   if (fileFormat === 'svg') {
     console.log('svg not processed with sharp');
     return;
   }
 
-  let sh = sharp('./input-head/' + file);
+  let sh = sharp('./inputheader/' + file);
   if (fileFormat === 'jpg' || fileFormat === 'jpeg') {
     sh = sh.jpeg({ quality: 80 });
     sh.resize({
@@ -31,8 +32,8 @@ filenames.forEach((file) => {
       position: sharp.strategy.entropy
     });
   }
-
-  sh.toFile('tumbnail/' + file, function (err, info) {
+  sh.toFormat("webp", { lossless: false, quality: 60, alphaQuality: 50 });
+  sh.toFile('img/tumbnail/' + fileName  + 'webp', function (err, info) {
     console.log(info);
     if (err) {
       console.log('error in image optimization');
@@ -44,4 +45,8 @@ filenames.forEach((file) => {
 function getExtension(filename) {
   let ext = path.extname(filename || '').split('.');
   return ext[ext.length - 1];
-}
+};
+function getName(filename) {
+  let ext = path.basename(filename || '', 'png');
+  return ext;
+};
