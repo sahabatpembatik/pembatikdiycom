8b55abccfffacec834e6267c5782e8cc772dfe6c
unknown <bhe.rifai07@gmail.com>
2024-06-13 00:31:26 +0700

    update Thu, Jun 13, 2024 12:31:25 AM

---
+++ themes/temabaru/assets/js/lazysizesconfig.js
@@ -0,0 +1,12 @@
+window.lazySizesConfig = {
+  rias: {
+      widths: [
+          270,
+          320,
+          372,
+          480,
+          560,
+          // ...
+      ]
+  }
+};
---
+++ themes/temabaru/assets/main/config.js
@@ -0,0 +1,11 @@
+{{- $params := $.Site.Params -}}
+window.params = {
+    codeBlock: {
+        maxLines: {{ default 7 $params.codeBlock.maxLines }},
+        lineNos: {{ default true $params.codeBlock.lineNos }},
+    },
+    utterances: {
+        theme: {{ default "" $params.utterances.theme | printf "%q" }},
+    },
+}
+localStorage.setItem("bs-palette", "sinau")
