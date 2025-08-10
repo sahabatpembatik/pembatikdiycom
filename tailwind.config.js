fcaf925372913df0d27c27c5648a029c55eb2506
Bakhtiar Rifai <bhe.rifai07@gmail.com>
2025-08-10 12:01:42 +0700

    update Sun, Aug 10, 2025 12:01:42 PM

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,24 @@ module.exports = {
     "./layouts/**/*.html",
     "./themes/pembatik2/layouts/**/*.html",
     "./themes/temabaru/layouts/**/*.html",
+    "./content/**/*.html",
   ],
+  theme: {
+    extend: {
+      animation: {
+        'wiggle': 'wiggle 1s ease-in-out infinite',
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' }
+        }
+      }
+    }
+  },
   darkMode: "class",
   plugins: [
     require('@tailwindcss/typography'),
   ],
+
 }
