05ed8a91241281597cfb1c908b28482c685c48e9
Bakhtiar Rifai <bhe.rifai07@gmail.com>
2025-11-12 23:03:57 +0700

    update Wed, Nov 12, 2025 11:03:57 PM

--- static/mpi-sistem-manusia/story_content/user.js
+++ static/mpi-sistem-manusia/story_content/user.js
@@ -2,6 +2,210 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "5oJzCrGXrdE":
+        Script1();
+        break;
+      case "5vnINSUa1vw":
+        Script2();
+        break;
+      case "5ZX0x9SkEFr":
+        Script3();
+        break;
+      case "6kq5sTHs6ej":
+        Script4();
+        break;
+      case "5xZHWEvat25":
+        Script5();
+        break;
+      case "5jeoXinPVy1":
+        Script6();
+        break;
+      case "6XYoRIWIuNI":
+        Script7();
+        break;
+      case "63Qt7mejE67":
+        Script8();
+        break;
+      case "6BTGdDkUbuU":
+        Script9();
+        break;
+      case "6jri2qBlbMY":
+        Script10();
+        break;
+      case "6STlIqW8Ln6":
+        Script11();
+        break;
+      case "5wGqz8EghIf":
+        Script12();
+        break;
+      case "6emU4lNtu6h":
+        Script13();
+        break;
+      case "5uZpkj7tOia":
+        Script14();
+        break;
+      case "6mRXMojI9M2":
+        Script15();
+        break;
+      case "5bhxyuzqrDh":
+        Script16();
+        break;
+      case "6M643u28lpu":
+        Script17();
+        break;
+      case "6qafnLggsdA":
+        Script18();
+        break;
   }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgsound');
+audio.src="musik.mp3";
+audio.load();
+audio.play();
+audio.volume=0.2;
+}
+
+function Script2()
+{
+  var audiomusik = document.getElementById('bgsound');
+audiomusik.volume=0;
+}
+
+function Script3()
+{
+  var audiomusik = document.getElementById('bgsound');
+audiomusik.volume=0.2;
+}
+
+function Script4()
+{
+  var audiobg = document.getElementById('bgsound');
+audiobg.volume=0;
+}
+
+function Script5()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script6()
+{
+  var audiomusik = document.getElementById('bgsound');
+audiomusik.volume=0.2;
+}
+
+function Script7()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script8()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script9()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script10()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script11()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script12()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script13()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script14()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script15()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script16()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script17()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
+}
+
+function Script18()
+{
+  var soundbtn = document.getElementById('soundpress');
+soundbtn.src="musik.mp3";
+soundbtn.load();
+soundbtn.play();
+audio.volume=1;
 }
 
